df28cd1ff50aa2690e224269f48b5fb5eb53b500
andela-jwarugu <joy.warugu@andela.com>
2016-12-09 11:42:09 +0300

    [Chore] Setup project structure

---
+++ tests/server/members.test.js
@@ -0,0 +1,7 @@
+const expect = require('chai').expect;
+
+describe('fetching members controller', () => {
+  it('returns a list of members from github', () => {
+    expect(true).to.be.true;
+  })
+})
