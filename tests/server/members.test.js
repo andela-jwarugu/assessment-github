578a24bb49ccf402572cd0ebc8c87ae7235ea5d0
andela-jwarugu <joy.warugu@andela.com>
2016-12-09 12:01:59 +0300

    [Feature] Add tests for the fetch of users

--- tests/server/members.test.js
+++ tests/server/members.test.js
@@ -1,7 +1,31 @@
 const expect = require('chai').expect;
+const app = require('../../index');
+const request = require('supertest')(app);
 
 describe('fetching members controller', () => {
-  it('returns a list of members from github', () => {
-    expect(true).to.be.true;
+  it('returns a list of members from github', (done) => {
+    request
+      .get('/api/members')
+      .end((err, res) => {
+        console.log(res.status);
+        expect(res.status).to.equal(200);
+        expect(res.body).to.exist;
+        expect(Array.isArray(res.body)).to.be.true;
+        expect(res.body).to.have.length(30);
+        done();
+      })
+  })
+
+  it('returns an object with specific user details', (done) => {
+    request
+      .get('/api/members/andela-jwarugu')
+      .end((err, res) => {
+        expect(res.status).to.equal(200);
+        expect(res.body).to.exist;
+        expect(res.body).to.be.an.object;
+        expect(res.body).to.have.keys('name', 'followers', 'following', 'repos');
+        expect(res.body.name).to.equal('andela-jwarugu');
+        done();
+      })
   })
 })
